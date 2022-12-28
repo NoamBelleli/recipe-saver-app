de1d52bbc2fd22aa305c55534b10158c30fbf618
Noam Belleli <noamhoter@gmail.com>
2022-12-28 17:57:26 +0200

    created error handling

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,35 +1,34 @@
 //@desc Get recipes
 //@route GET /api/recipes
 //@access Private
-
-
-const getRecipes = (req, res) => {
+const getRecipes = async (req, res) => {
   res.status(200).json({message: 'Get recipes'})
 }
 
 //@desc Create recipe
 //@route POST /api/recipes
 //@access Private
-
-const createRecipe = (req, res) => {
+const createRecipe = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+  
   res.status(200).json({ message: 'Create recipe' })
 }
 
 //@desc Edit recipe
 //@route PUT /api/recipes/:id
 //@access Private
-
-const editRecipe = (req, res) => {
+const editRecipe = async (req, res) => {
   res.status(200).json({message: `Edit recipe ${req.params.id}`})
 }
 
 //@desc Delete recipe
 //@route DELETE /api/recipes/:id
 //@access Private
-
-const deleteRecipe = (req, res) => {
+const deleteRecipe = async (req, res) => {
   res.status(200).json({message: `delete recipe ${req.params.id}`})
 } 
-
 
 export { getRecipes, createRecipe, editRecipe, deleteRecipe } ;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import router from './routes/recipeRoutes.js';
+import { errorHandler } from './middleware/errorMiddlewar.js';
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/recipes', router);
+
+app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`server started on port ${port}`))
 
