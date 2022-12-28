4ae05d6b10170b93e8711045423d699836f4f509
Noam Belleli <noamhoter@gmail.com>
2022-12-28 18:02:43 +0200

    import and initial express async handler

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,34 +1,35 @@
+import asyncHandler from 'express-async-handler'
+
 //@desc Get recipes
 //@route GET /api/recipes
 //@access Private
-const getRecipes = async (req, res) => {
+const getRecipes = asyncHandler(async (req, res) => {
   res.status(200).json({message: 'Get recipes'})
-}
+})
 
 //@desc Create recipe
 //@route POST /api/recipes
 //@access Private
-const createRecipe = async (req, res) => {
+const createRecipe = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
-  }
-  
+  }  
   res.status(200).json({ message: 'Create recipe' })
-}
+})
 
 //@desc Edit recipe
 //@route PUT /api/recipes/:id
 //@access Private
-const editRecipe = async (req, res) => {
+const editRecipe = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Edit recipe ${req.params.id}`})
-}
+})
 
 //@desc Delete recipe
 //@route DELETE /api/recipes/:id
 //@access Private
-const deleteRecipe = async (req, res) => {
+const deleteRecipe = asyncHandler(async (req, res) => {
   res.status(200).json({message: `delete recipe ${req.params.id}`})
-} 
+} )
 
 export { getRecipes, createRecipe, editRecipe, deleteRecipe } ;
